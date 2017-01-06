ef094603b043db5f10958c428b1b06cba91c6780
thaihaluong <halt68.ftu@gmail.com>
2017-01-06 20:29:41 +0700

    LTH.

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('main.html')
+
+@app.route('/create')
+def create():
+    return "Hello Jane"
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
